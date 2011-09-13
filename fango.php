22582c957991f40028262062db51c24334246aa2
Andrea Giardina <andrea.giardina@gmail.com>
2011-09-13 16:59:18 +0200

    Fango copy changed

--- fango.php
+++ fango.php
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @author Andrea Giardina <andrea.giardina@crealabs.it>
+ * @author Andrea Giardina <andrea.giardina@gmail.com>
  * @link http://github.com/agiardina/Fango
  */
 
